4876739a1592df5c26bb3edcb795b98040921d15
julianxlozano <julianxlozano@gmail.com>
2021-04-17 18:17:58 -0500

    commit tree 7548b34a0fbe016b37287ea82f57bff3b8fac828

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,11 @@
 class Student < ActiveRecord::Base
+  has_one :address
+  after_initialize :init
+
+  def init 
+    self.active = false if self.active.nil?
+  end
+
   def to_s
     self.first_name + " " + self.last_name
   end
